235d0ffea46355d531db37aa00926cc416f48a2e
flyan123 <2495365901@qq.com>
2024-07-15 23:58:39 +0800

    add 0715

--- src/views/layout/content/contentPage.vue
+++ src/views/layout/content/contentPage.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
-        右侧内容 
+        <!-- 顶部  -->
+         <HeaderPage/>
+         <!-- 内容 -->
+          <router-view> </router-view>
     </div>
 </template>
+<script>
+import HeaderPage from './headerPage.vue';
+export default {
+    components:{
+        HeaderPage
+    }
+}
+</script>
